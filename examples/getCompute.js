903f8fda8516cb8d0d0b61aa8f19e86e3841f758
kotesla <kotesla@outlook.com>
2026-04-05 13:01:53 +0300

    First

--- examples/getCompute.js
+++ examples/getCompute.js
@@ -45,13 +45,14 @@ const stdDev = 3;
 // should be dealt with separately.
 const pcOutliersUser = 15;
 
-// Server uses public key to find the matching private key
-// to be able to decode and encode message payload.
+// Public key to encode data in transit. Although secure HTTPS
+// transport is used by default, some users prefer an extra security
+// layer they can control.
 const publicKey = "public_key_here"; // private key (from token)
 
-// Private key is not sent over the network. It is used to scramble
-// the payload for transit. Although secure HTTPS transport is used
-// by default, many users prefer an extra security layer they can control.
+// Private key is not sent over the network, it is used to scramble payload
+// when in transit. Although secure HTTPS transport is used by default,
+// some users prefer an extra security layer they can control.
 const privateKey = "private_key_here"; // private key (from token)
 
 // *********************************************************************
--- examples/getErrorModelListShort.js
+++ examples/getErrorModelListShort.js
@@ -5,16 +5,15 @@ const { printObj, throwArity } = require("../shared/utils");
 // *********************** FUNCTION ARGUMENTS **************************
 // *********************************************************************
 
-// Token consists of public and private keys.
 // Public key to encode data in transit. Although secure HTTPS
 // transport is used by default, some users prefer an extra security
 // layer they can control.
-const publicKey = "public_key_here"; // private key (token)
+const publicKey = "public_key_here"; // private key (from token)
 
 // Private key is not sent over the network, it is used to scramble payload
 // when in transit. Although secure HTTPS transport is used by default,
 // some users prefer an extra security layer they can control.
-const privateKey = "private_key_here"; // private key (token)
+const privateKey = "private_key_here"; // private key (from token)
 
 // *********************************************************************
 // ************************ FUNCTION TO RUN ****************************
